6e808c5dbd4d524ed7cf487e2a8712bf6415dfdc
Dudnyk <dddvvvvvv07@gmail.com>
2021-04-24 02:25:30 +0300

    first commit

---
+++ assets/js/color-switch.js
@@ -0,0 +1,34 @@
+// Есть массив цветов в hex-формате и кнопки Start и Stop.
+// Напиши скрипт, который после нажатия кнопки Start, раз в секунду меняет цвет фона body на случайное значение из массива
+// используя инлайн-стиль. При нажатии на кнопку Stop, изменение цвета фона должно останавливаться.
+// Учти, на кнопку Start можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение темы запушено, кнопка Start была не активна.
+// Для генерации случайного числа (индекс элемента массива цветов), используй функцию randomIntegerFromInterval.
+
+const colors = ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"];
+
+const bodyEl = document.querySelector("body");
+const startBtnEl = document.querySelector('button[data-action="start"]');
+const stopBtnEl = document.querySelector('button[data-action="stop"]');
+
+startBtnEl.addEventListener("click", onStartChangeColor);
+stopBtnEl.addEventListener("click", onStopChangeColor);
+
+let intervalId = null;
+
+function onStartChangeColor() {
+  intervalId = setInterval(onChangeColor, 1000);
+  startBtnEl.disabled = true;
+}
+
+function onChangeColor() {
+  bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
+}
+
+function onStopChangeColor() {
+  clearInterval(intervalId);
+  startBtnEl.disabled = false;
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
---
+++ assets/js/timer.js
@@ -0,0 +1,59 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.intervalId = null;
+    this.selector = document.querySelector(selector);
+    this.targetDate = targetDate;
+
+    this.refs = {
+      daysEl: this.selector.querySelector('[data-value="days"]'),
+      hoursEl: this.selector.querySelector('[data-value="hours"]'),
+      minsEl: this.selector.querySelector('[data-value="mins"]'),
+      secsEl: this.selector.querySelector('[data-value="secs"]'),
+    };
+  }
+
+  start() {
+    this.updateTime(this.targetDate);
+
+    const timerId = setInterval(() => {
+      if (new Date() > this.targetDate) {
+        clearInterval(timerId);
+        alert("Время истекло!");
+      }
+      this.updateTime(this.targetDate);
+    }, 1000);
+  }
+
+  updateTime(date) {
+    const deltaTime = date - new Date();
+    const remaningTime = this.getTimeComponents(deltaTime);
+    this.updateTextContent(remaningTime);
+  }
+
+  updateTextContent({ days, hours, mins, secs }) {
+    this.refs.daysEl.textContent = days;
+    this.refs.hoursEl.textContent = hours;
+    this.refs.minsEl.textContent = mins;
+    this.refs.secsEl.textContent = secs;
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+const newTimer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("May 18, 2021"),
+});
+
+newTimer.start();
